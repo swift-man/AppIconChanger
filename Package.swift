4c9bfafdf939af889b50fea2f93b68a3e0fb8553
swift-man <jiniopening@naver.com>
2026-06-14 04:24:17 +0900

    ci: add DocC deployment workflow

--- Package.swift
+++ Package.swift
@@ -6,12 +6,16 @@ let package = Package(
   name: "AppIconChanger",
   platforms: [
     .iOS(.v13),
+    .macOS(.v10_15),
   ],
   products: [
     .library(
       name: "AppIconChanger",
       targets: ["AppIconChanger"]
     ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
   ],
   targets: [
     .target(
